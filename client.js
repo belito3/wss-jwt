d6eb1fcc9625e5dd2175cbb0573351a23bfe8430
bao <bao.buivan3@gmail.com>
2018-10-26 23:19:09 +0700

    tiny modify

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ var jwt = require('jsonwebtoken');
 var config = require('./config');
 var WebSocket = require('ws');
 
-var token = jwt.sign({name: 'iostreamer'}, config.jwtSecretKey, {
+var token = jwt.sign({name: 'iostreamer'}, config.JWT_SECRETKEY, {
     expiresIn: 15 * 24* 60 * 60 * 1000 // 15 days 
 })
 
